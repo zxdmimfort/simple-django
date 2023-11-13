4042e3029ddea67795e1b2b6002fe944187f417e
mimfort <mimfort21@gmail.com>
2023-11-13 20:16:34 +0300

    add custom 404 page

--- nc/workers/views.py
+++ nc/workers/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -11,8 +11,18 @@ def categories(request: HttpRequest, cat_id: int):
 
 
 def categories_by_slug(request: HttpRequest, cat_slug):
-    return HttpResponse(f"<h1>Categories by slug:</h1><p>id: {cat_slug}</p>")
+    if request.GET:
+        print(request.GET)
+    elif request.POST:
+        print(request.POST)
+    return HttpResponse(f"<h1>Categories by slug:</h1><p>slug: {cat_slug}</p>")
 
 
 def archive(request: HttpRequest, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")

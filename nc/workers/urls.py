244bb27282133344bf3a21620bc914ab278eda1e
mimfort <mimfort21@gmail.com>
2023-11-13 14:57:06 +0300

    add register_converter instead of re_path

--- nc/workers/urls.py
+++ nc/workers/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from workers import views
+from django.urls import path, re_path, register_converter
+from . import views
+from . import converters
 
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index),
-    path('categories/', views.categories),
+    path('cats/<int:cat_id>/', views.categories), # http://127.0.0.1:8000/cats/2/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/cats/sdg2/
+    path("archive/<year4:year>/", views.archive),
 ]
--- nc/workers/views.py
+++ nc/workers/views.py
@@ -6,5 +6,13 @@ def index(request: HttpRequest):
     return HttpResponse("Страница приложения workers.")
 
 
-def categories(request: HttpRequest):
-    return HttpResponse("<h1>HAHAHA</h1>")
+def categories(request: HttpRequest, cat_id: int):
+    return HttpResponse(f"<h1>Categories:</h1><p>id: {cat_id}")
+
+
+def categories_by_slug(request: HttpRequest, cat_slug):
+    return HttpResponse(f"<h1>Categories by slug:</h1><p>id: {cat_slug}</p>")
+
+
+def archive(request: HttpRequest, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")

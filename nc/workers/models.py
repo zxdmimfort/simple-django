fd2db34b30ccea77fa400b99b0afa3166e0ba4b6
mimfort <mimfort21@gmail.com>
2023-11-29 00:53:48 +0300

    Add django-extensions and default order_by in orm

--- nc/workers/models.py
+++ nc/workers/models.py
@@ -7,3 +7,12 @@ class Worker(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create']),
+        ]
+
+    def __str__(self):
+        return self.title + str(self.time_create)
